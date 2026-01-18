0936048841e9079099ccc3bfde547e6d29163d40
rmxway <rmxway@mail.ru>
2026-01-18 14:26:17 +0300

    Enhance Redux provider to reset currency to USD on initialization and refactor exchange rate hook to simplify subscriber notifications

--- store/provider.tsx
+++ store/provider.tsx
@@ -1,15 +1,29 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { persistor, store } from '@/store';
+import { resetCurrencyToUSD } from '@/store/reducers/products';
 
-export const ReduxProvider = ({ children }: { children: ReactNode }) => (
-	<PersistGate persistor={persistor}>
-		<Provider store={store}>{children}</Provider>
-	</PersistGate>
-);
+export const ReduxProvider = ({ children }: { children: ReactNode }) => {
+	useEffect(() => {
+		// Сбрасываем валюту на USD при инициализации приложения
+		if (persistor) {
+			store.dispatch(resetCurrencyToUSD());
+		}
+	}, []);
+
+	if (!persistor) {
+		return <Provider store={store}>{children}</Provider>;
+	}
+
+	return (
+		<PersistGate persistor={persistor}>
+			<Provider store={store}>{children}</Provider>
+		</PersistGate>
+	);
+};
 
 export default ReduxProvider;
