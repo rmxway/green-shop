d3096bffee6a81dd1525deddc951fb13613bc43d
rmxway <rmxway@mail.ru>
2026-01-29 23:50:06 +0300

    Remove .env file, update Next.js to version 16.1.6, and adjust package dependencies in package.json and yarn.lock.

--- next.config.js
+++ next.config.js
@@ -18,6 +18,7 @@ module.exports = {
 	compiler: {
 		styledComponents: true,
 	},
+	turbopack: {},
 	webpack(config) {
 		config.resolve.fallback = {
 			...config.resolve.fallback, // if you miss it, all the other options in fallback, specified by next.js will be dropped. Doesn't make much sense, but how it is
